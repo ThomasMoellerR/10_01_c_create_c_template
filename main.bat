ec8d070501a3eb50aaf3aae6e194f89b6689656e
test <test>
2019-02-14 18:33:05 +0100

    test

---
+++ main.bat
@@ -0,0 +1,4 @@
+@echo off
+cmd /k py C:\Users\computer\Downloads\create_c_template-master\create_c_template-master\main.py ^
+--output_folder "C:\Users\computer\Downloads\create_c_template-master\create_c_template-master\output" ^
+--modul_name "ser" ^
