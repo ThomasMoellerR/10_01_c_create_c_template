4ca719464b6e4b37d9d4a7080efca3381889ccaa
test <test>
2019-02-14 20:01:33 +0100

    test

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import argparse
 import time
 import os
+import time
 
 nl = "\n" # newline
+ss = "\"" # string sign
 
 
 def open_sector():
@@ -20,17 +22,22 @@ def close_sector():
 	a += nl
 	return a
 
-def write():
+def abstand():
 	a = ""
 	a += "*" 
 	for i in range(2):
 		a += " "
 	return a
 
+def write_text(text):
+	a = ""
+	a += abstand()
+	a += "{:15s}:  ".format(text)
+	return a
 
 def insert_sector_name(sector_name):
 	a = ""
-	a += write()
+	a += abstand()
 	a += sector_name
 	a += nl
 	return a
@@ -44,11 +51,13 @@ def create_sector(sector_name):
 	return a
 
 def create_header(name, typ, version): # Kopf
+	t = time.strftime("%Y-%m-%d %H:%M:%S")
 	a = ""
 	a += nl
 	a += open_sector()
-	a += write() + "File           :  " + name + typ + nl
-	a += write() + "Version        :  " + version + nl
+	a += write_text("File") + name + typ + nl
+	a += write_text("Version") + version + nl
+	a += write_text("Last Changes") + t + nl
 	a += close_sector()
 	return a
 
@@ -67,7 +76,6 @@ def create_compiler_switch_start(name):
 
 def create_compiler_switch_end(name):
 	a = ""
-	a += nl
 	a += "#endif /* __"
 	a += name.upper()
 	a += "_H */"
@@ -98,12 +106,11 @@ def create_changes_section():
 	a = ""
 	a += nl
 	a += open_sector()
-	a += write() + nl
-	a += write()
-	a += "Changes        :  " + nl
-	a += write() + nl
-	a += write() + nl
-	a += write() + nl
+	a += abstand() + nl
+	a += write_text("Changes") + nl
+	a += abstand() + nl
+	a += abstand() + nl
+	a += abstand() + nl
 	a += close_sector()
 	return a
 
@@ -112,10 +119,10 @@ def create_function_description():
 	a = ""
 	a += nl
 	a += open_sector()
-	a += write() + "Function Name  :  " + nl
-	a += write() + "Description    :  " + nl
-	a += write() + "Parameter(s)   :  " + nl
-	a += write() + "Return Value   :  " + nl
+	a += write_text("Function Name") + nl
+	a += write_text("Description") + nl
+	a += write_text("Parameter(s)") + nl
+	a += write_text("Return Value") + nl
 	a += close_sector()
 	return a
 
@@ -123,9 +130,22 @@ def end_of_file(name):
 	a = ""
 	a += nl
 	a += open_sector()
-	a += write() + "END OF FILE    :  " + name + ".h" + nl
+	a += write_text("END OF FILE") + name + ".h" + nl
 	a += close_sector()
 	return a	
+
+def create_extern_c_start():
+	a = ""
+	a += nl
+	a += "extern " + ss + "C" + ss + " {" + nl
+	return a
+
+def create_extern_c_end():
+	a = ""
+	a += nl
+	a += "}" + nl
+	a += nl
+	return a
 
 
 # Argparse
@@ -139,6 +159,7 @@ args = parser.parse_args()
 a = ""
 a += create_header(args.modul_name, ".h","1")
 a += create_compiler_switch_start(args.modul_name)
+a += create_extern_c_start()
 a += create_sector("Include Files")
 a += create_sector("Global Constants")
 a += create_sector("Global Type Definitions")
@@ -146,6 +167,7 @@ a += create_sector("Global Variables")
 a += create_sector("Global Function Prototypes")
 a += create_ini_prototyp(args.modul_name)
 a += create_changes_section()
+a += create_extern_c_end()
 a += create_compiler_switch_end(args.modul_name)
 a += end_of_file(args.modul_name)
 
@@ -179,3 +201,5 @@ f.write(a)
 f.close()
 
 #print(a)
+
+
